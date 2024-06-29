1548ab87ec674719b1ef86fa6b7dd0fa331e4338
Md. Shohanur Rahman Shohan <89014168+encodeshohan@users.noreply.github.com>
2024-06-29 16:26:58 +0600

    Add files via upload

---
+++ Basic-Exercise/problem_02.py
@@ -0,0 +1,7 @@
+'''
+Find and print the python version that you are using.
+Use sys.version (module sys) or platform.python_version() (module platform) to find the version.
+'''
+
+import sys
+print("Python version:", sys.version)
