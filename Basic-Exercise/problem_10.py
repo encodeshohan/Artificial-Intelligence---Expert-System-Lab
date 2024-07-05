eeea9dbd8ed51f529c6267d4ea42edc3e51c032c
Md. Shohanur Rahman Shohan <89014168+encodeshohan@users.noreply.github.com>
2024-07-05 13:20:30 +0600

    Add files via upload

---
+++ Basic-Exercise/problem_10.py
@@ -0,0 +1,9 @@
+'''
+Write a Python program to count the number of occurrences of a specific character in a string.
+[try using count()]
+'''
+
+text = "Hello AI"
+search = 'l'
+count = text.count(search)
+print("Number of occurrences of", search, ":", count)
